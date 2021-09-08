b9e1d8c9cd037a4a34b414b7aa37c20993520f98
innerpeacez <innerpeace.zhai@gmail.com>
2021-09-08 11:14:54 +0800

    id

--- agent-core/src/main/java/cloud/erda/agent/core/utils/Constants.java
+++ agent-core/src/main/java/cloud/erda/agent/core/utils/Constants.java
@@ -116,7 +116,7 @@ public class Constants {
 
         public static final String DB_STATEMENT = "db_statement";
 
-        public static final String DB_STATEMENT_ID = "db_statement";
+        public static final String DB_STATEMENT_ID = "db_statement_id";
 
         public static final String DB_INSTANCE = "db_instance";
 
