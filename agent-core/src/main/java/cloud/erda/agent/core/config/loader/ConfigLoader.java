f794de418152702dcfc2e9227f9246e8eb70cf36
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-11-04 16:25:16 +0800

    code cleanup

--- agent-core/src/main/java/cloud/erda/agent/core/config/loader/ConfigLoader.java
+++ agent-core/src/main/java/cloud/erda/agent/core/config/loader/ConfigLoader.java
@@ -57,7 +57,6 @@ public abstract class ConfigLoader {
                     if (!validateValue(item.getKey(), item.getValue(), field)) {
                         continue;
                     }
-//                    log.debug("Try matches {} map config {} item {}={} from {}", instance.getClass().getSimpleName(), mapConfiguration.pattern(), item.getKey(), item.getValue(), getClass().getSimpleName());
                     if (item.getKey().matches(mapConfiguration.pattern())) {
                         Object value = ReflectionUtils.castValue(item.getValue(), mapConfiguration.valueType());
                         map.put(item.getKey(), value);
@@ -94,17 +93,4 @@ public abstract class ConfigLoader {
             log.info("!Error. Load {} config {} from {}", instance.getClass().getSimpleName(), name, getClass().getSimpleName());
         }
     }
-
-//    private void loadValue(String name, String value, Object instance, Field field) {
-//        if (validateValue(name, value, instance, field)) {
-//            try {
-//                field.setAccessible(true);
-//                Object fieldValue = ReflectionUtils.castValue(value, field.getType());
-//                field.set(instance, fieldValue);
-//                log.info("Load {} config {}={} from {}", instance.getClass().getSimpleName(), name, fieldValue, getClass().getSimpleName());
-//            } catch (IllegalAccessException e) {
-//                log.info("!Error. Load {} config {}={} from {}", instance.getClass().getSimpleName(), name, value, getClass().getSimpleName());
-//            }
-//        }
-//    }
 }
