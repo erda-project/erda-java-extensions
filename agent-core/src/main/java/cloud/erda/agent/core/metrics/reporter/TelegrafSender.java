f507e893e2756d3d07d67ad93e57ab429c1f2b21
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-11-11 15:48:24 +0800

    Fix limit bug

--- agent-core/src/main/java/cloud/erda/agent/core/metrics/reporter/TelegrafSender.java
+++ agent-core/src/main/java/cloud/erda/agent/core/metrics/reporter/TelegrafSender.java
@@ -40,7 +40,7 @@ import java.util.Properties;
 public class TelegrafSender implements IConsumer<Metric> {
 
     private static final int DEFAULT_BUCKET = 10;
-    private static final int UDP_DATA_LIMIT = 1024 * 64;
+    private static final int UDP_DATA_LIMIT = 1024 * 64 - 1;
 
     private static final ILog log = LogManager.getLogger(MetricDispatcher.class);
 
@@ -100,7 +100,7 @@ public class TelegrafSender implements IConsumer<Metric> {
 
     private void doSend(Metric[] buckets) {
         byte[] data = GsonUtils.toBytes(buckets);
-        if (buckets.length >= UDP_DATA_LIMIT) {
+        if (data.length >= UDP_DATA_LIMIT) {
             send(buckets, Math.max(buckets.length / 2, 1));
             return;
         }
